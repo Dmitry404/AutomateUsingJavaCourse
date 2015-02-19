2d20a5f97dcb05cceadd3ba7191e9666050c455d
Gennady Tsapock <gennady.tsapock@hp.com>
2015-02-19 18:30:17 +0200

    Added users support

---
+++ gtsapock/src/User.java
@@ -0,0 +1,14 @@
+/**
+ * Created by gtsapock on 2015-02-19.
+ */
+
+import sun.security.util.Password;
+
+import java.util.Date;
+
+public class User {
+    Date joinDate;
+    String nickName;
+    Password pwd;
+    public Boolean isBanned;
+}
