5d96abd2b44a08305fd5302ece061733e62fc0a3
Hedg <hedgss@gmail.com>
2015-06-05 13:37:20 +0300

    Update DemoApplication.java

--- Shevtsov/src/main/java/demo/DemoApplication.java
+++ Shevtsov/src/main/java/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class DemoApplication extends SpringBootServletInitializer {
 
     @Override
